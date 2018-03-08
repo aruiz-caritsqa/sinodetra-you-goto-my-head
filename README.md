5ffb2a946bdc81d5f03d05874c906c88ba028118
Adrien Ruiz <ruiz.adrien@gmail.com>
2018-03-08 16:46:45 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # sinodetra-you-goto-my-head
-Sinodetra server by fapprik with the added ability to respond with custom headers
+
+> Minimal Sinatra like router for node.js by fapprik... with the added ability to respond with custom headers
+
+## Installation
+`npm install sinodetra-you-goto-my-head`
+
+## Usage
+
+At first, (install and) require sinodetra-you-goto-my-head within your script and specify your preferred port (`8000` in this example).
+
+`var app = require('sinodetra-you-goto-my-head')(8000)`
+
+You can refer to [Sinodetra README](https://github.com/fapprik/sinodetra) for a better idea on usage
+
+## Examples
+```javascript
+app.get('/', function(request, response) {
+	response.plain('Hello world!')
+})
+
+app.get('/say/:greeting/to/:person', function(request, response, greeting, person) {
+	response.html('<b>' + greeting + '</b>, ' + person + '!')
+})
+
+app.get('/([0-9]+)/plus/([0-9]+)', function(request, response, one, two) {
+	response.plain((parseInt(one, 10) + parseInt(two, 10)).toString())
+})
+
+app.error(function(request, response) {
+	response.plain('404')
+})
+
+// In order to resolve the CORS error when running the server locally
+app.get('/healthcheck', (request, response) => {
+  console.log(request)
+	response.send(
+    {active: true},
+    200,
+    'application/json',
+    {'Access-Control-Allow-Origin': "*", 'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE'}
+  )
+})  
+```
+
+## Contributors
+- [fapprik](http://fapprik.com/)
+- [@rasshofer](https://github.com/rasshofer)
+- [@paulcockrell](https://github.com/paulcockrell)
---
+++ index.js
@@ -0,0 +1,110 @@
+#!/usr/bin/env node
+
+/*
+ * sinodetra-you-goto-my-head
+ * Based on Sinodetra => https://github.com/fapprik/sinodetra
+ */
+
+'use strict';
+
+module.exports = function(port) {
+
+  var routes = { };
+  var error;
+  var http = require('http');
+  var url = require('url');
+  var queryString = require('querystring');
+  var server = http.createServer();
+  var parse = url.parse;
+  var placeholders = /(:\w+)/gi;
+
+  ['GET', 'POST', 'PUT', 'DELETE'].forEach(function(method) {
+    http.Server.prototype[method.toLowerCase()] = function(path, callback) {
+      if (typeof routes[method] !== 'object') {
+        routes[method] = {};
+      }
+      if (placeholders.test(path)) {
+        path = path.replace(placeholders, function() {
+          return '([^\/]+)';
+        });
+      }
+      routes[method][path] = callback;
+    }
+  });
+
+  http.Server.prototype['error'] = function(callback) {
+    error = callback;
+  };
+
+  http.ServerResponse.prototype.send = function(message, code, type, headers = {}) {
+    code = code || 200;
+    type = type || 'text/html';
+    if (typeof message === 'object') {
+      message = JSON.stringify(message);
+      type = 'application/json';
+    }
+
+    headers['Content-Type'] = type
+
+    this.writeHead(code, headers);
+    this.end(message);
+  };
+
+  http.ServerResponse.prototype.html = function(message, code) {
+    this.send(message, 'text/html', code);
+  };
+
+  http.ServerResponse.prototype.plain = function(message, code) {
+    this.send(message, 'text/plain', code);
+  };
+
+  http.ServerResponse.prototype.json = function(message, code) {
+    if (typeof message !== 'string') {
+      message = JSON.stringify(message);
+    }
+    this.send(message, 'application/json', code);
+  };
+
+  server.on('request', function(request, response) {
+    var pathname = parse(request.url).pathname;
+    if (typeof routes[request.method] === 'object') {
+      for (var route in routes[request.method]) {
+        var matches = pathname.match('^' + route + '$');
+        if (matches) {
+          var args = [request, response];
+          matches.shift();
+          matches.forEach(function(match) {
+            args.push(match);
+          });
+          var handle = routes[request.method][route];
+          request.body = '';
+          request.on('data', function(data) {
+            request.body += data;
+          });
+          request.params = url.parse(request.url, true).query || {};
+          request.param = function(key) {
+            return key in request.params ? request.params[key] : false;
+          };
+          request.on('end', function() {
+            try {
+              request.body = JSON.parse(request.body);
+            } catch (e) {
+              var query = queryString.parse(request.body) || {};
+              for (var attr in query) {
+                request.params[attr] = query[attr];
+              }
+            }
+            handle.apply(this, args);
+          });
+          return;
+        }
+      }
+    }
+    if (typeof error === 'function') {
+      error.apply(this, arguments);
+    }
+  });
+
+  return server.listen(port);
+
+};
